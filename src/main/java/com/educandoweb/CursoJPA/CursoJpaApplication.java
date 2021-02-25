268389aa8e0a49c6c1b28c9ff95e390d39921c92
Solismar Vicente <solismarvicente@gmail.com>
2021-02-25 15:32:24 -0300

    Projeto CursoJPA criado

---
+++ src/main/java/com/educandoweb/CursoJPA/CursoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.CursoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/CursoJPA/CursoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.CursoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
