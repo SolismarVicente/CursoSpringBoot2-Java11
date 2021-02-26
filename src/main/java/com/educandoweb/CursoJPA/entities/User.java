7fd3646dc96542ccaa8bdcc103f675f90e91f81d
SolismarVicente <solismarvicente@gmail.com>
2021-02-26 12:41:43 -0300

    Client@DESKTOP-A7EG19C MINGW64 /c/Cursos de Java/Sistemas/CursoDeJava/Modulo24/ $ git add . warning: LF will be replaced by CRLF in .gitignore. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in .mvn/wrapper/MavenWrapperDownloader.java. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in .mvn/wrapper/maven-wrapper.properties. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in mvnw. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in mvnw.cmd. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in pom.xml. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/main/java/com/educandoweb/CursoJPA/CursoJpaApplication.java. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/main/resources/application.properties. The file will have its original line endings in your working directory warning: LF will be replaced by CRLF in src/test/java/com/educandoweb/CursoJPA/CursoJpaApplicationTests.java. The file will have its original line endings in your working directory
    Client@DESKTOP-A7EG19C MINGW64 /c/Cursos de Java/Sistemas/CursoDeJava/Modulo24/Workspace/CursoJPA (master)
    $ git commit -m

--- src/main/java/com/educandoweb/CursoJPA/entities/User.java
+++ src/main/java/com/educandoweb/CursoJPA/entities/User.java
@@ -2,10 +2,18 @@ package com.educandoweb.cursojpa.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	private String nomeUser;
 	private String emailUser;
@@ -15,7 +23,7 @@ public class User implements Serializable {
 	public User() {
 		
 	}
-
+	
 	public User(Long codigo, String nomeUser, String emailUser, String telefoneUser, String senhaUser) {
 		super();
 		this.codigo = codigo;
