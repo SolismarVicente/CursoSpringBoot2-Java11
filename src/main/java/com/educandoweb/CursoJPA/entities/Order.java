e1a6663021fcf8fbe988e7dc5bec3c81dff2324f
SolismarVicente <solismarvicente@gmail.com>
2021-02-28 21:25:23 -0300

    Entidade pedido - parte 1

---
+++ src/main/java/com/educandoweb/CursoJPA/entities/Order.java
@@ -0,0 +1,93 @@
+package com.educandoweb.cursojpa.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+//Order: Pedido
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	private Instant instante;
+	
+	//Implementação do relacionamento entre Order(pedido) e
+	//User (usuario)
+	@ManyToOne
+	@JoinColumn(name = "client_codigo")
+	private User client;
+	
+	public Order() {
+		
+	}
+
+	public Order(Long codigo, Instant instante, User client) {
+		super();
+		this.codigo = codigo;
+		this.instante = instante;
+		this.client = client;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Instant getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Instant instante) {
+		this.instante = instante;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
+}
